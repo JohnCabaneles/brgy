6e3639c82fe5ba5fb0d7dc064e055476dcf6052b
John-WD31 <j.cabs8888@gmail.com>
2024-05-22 17:43:31 +0800

    store function added

--- resources/views/barangayId/index.blade.php
+++ resources/views/barangayId/index.blade.php
@@ -4,7 +4,7 @@
     <div class="mb-6">
       <h1 class="text-2xl">Fill up the form to add new Barangay ID.</h1>
     </div>
-    <form method="POST" action="{{-- {{ route('official.store') }} --}}" enctype="multipart/form-data">
+    <form method="POST" action="{{ route('barangayId.store') }}" enctype="multipart/form-data">
         @csrf
       <div class="-mx-3 grid grid-cols-4 mb-6">
 
@@ -51,17 +51,38 @@
         </div>
 
         <div class="md:w-1/2 lg:w-full px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="position">
-              Position
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="address">
+              Address
             </label>
-            <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" name="position" id="position" type="text" placeholder="Please enter position">
+            <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" name="address" id="address" type="text" placeholder="Please enter address">
         </div>
 
         <div class="md:w-1/2 lg:w-full px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="idNumber">
-              ID #
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="apartment">
+              Apartment,etc.
             </label>
-            <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" name="idNumber" id="idNumber" type="text" placeholder="Please enter id number">
+            <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" name="apartment" id="apartment" type="text" placeholder="Please enter apartment">
+        </div>
+
+        <div class="md:w-1/2 lg:w-full px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="city">
+              City
+            </label>
+            <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" name="city" id="city" type="text" placeholder="Please enter city">
+        </div>
+
+        <div class="md:w-1/2 lg:w-full px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="province">
+              Province
+            </label>
+            <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" name="province" id="province" type="text" placeholder="Please enter province">
+        </div>
+
+        <div class="md:w-1/2 lg:w-full px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="zipCode">
+              Zip Code
+            </label>
+            <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" name="zipCode" id="zipCode" type="text" placeholder="Please enter address">
         </div>
 
       </div>
@@ -71,11 +92,11 @@
   <hr />
   <div class="pt-2">
       <div class="table w-full p-2">
-          <h1 class="pb-3 text-2xl">All Category</h1>
+          <h1 class="pb-3 text-2xl">All IDs</h1>
           <table class="w-full border">
               <thead>
                   <tr class="bg-gray-50 border-b">
-                    <th class="p-2 border-r w-1/12 text-sm font-thin text-gray-500">
+                    <th class="p-2 border-r w-2/12 text-sm font-thin text-gray-500">
                         <div class="flex items-center justify-center">
                             ID #
                         </div>
@@ -89,7 +110,7 @@
 
                     <th class="p-2 border-r w-3/12 text-sm font-thin text-gray-500">
                         <div class="flex items-center justify-center">
-                            Position
+                            Email
                         </div>
                     </th>
 
@@ -109,9 +130,10 @@
               <tbody>
                     @foreach( $barangayIds as $key => $barangayId)
                         <tr class="bg-gray-100 text-center border-b text-sm text-gray-600">
-                            <td class="border-r">{{ $barangayId->id}}</td>
-                            <td class="border-r">{{ $barangayId->name}}</td>
-                            <td class="border-r">{{ $barangayId->contactNumber}} | {{ $barangayId->email}}</td>
+                            <td class="border-r">{{ $barangayId->brgy_id }}</td>
+                            <td class="border-r">{{ $barangayId->firstName}} {{$barangayId->lastName}}</td>
+                            <td class="border-r"> {{ $barangayId->email}}</td>
+                            <td class="border-r">{{ $barangayId->contactNumber}}</td>
                             <td class="flex justify-center space-x-2 p-2">
                                 <a href="{{ route('barangayId.edit', $barangayId->id)}}">
                                     <button class="bg-blue-500 hover:bg-blue-600 p-2 text-white hover:shadow-lg rounded-lg text-xs font-thin">Edit</button>
