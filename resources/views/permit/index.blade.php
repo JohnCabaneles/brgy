49a1c51edb95a4a83cca03ce674a3887ce830572
Andres Elacion III <andreselacioniii@gmail.com>
2024-06-03 18:05:16 +0800

    added payment button

--- resources/views/permit/index.blade.php
+++ resources/views/permit/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="pt-2">
     <div class="table w-full p-2">
-        <h1 class="pb-3 text-2xl">All officials</h1>
+        <h1 class="pb-3 text-2xl">All Permit</h1>
         <table class="w-full border">
             <thead>
                 <tr class="bg-gray-50 border-b">
@@ -30,6 +30,12 @@
                       </div>
                   </th>
 
+                  <th class="p-2 border-r w-3/12 text-sm font-thin text-gray-500">
+                    <div class="flex items-center justify-center">
+                        Payment
+                    </div>
+                  </th>
+
                   <th class="p-2 border-r text-sm font-thin text-gray-500">
                       <div class="flex items-center justify-center">
                           Action
@@ -42,8 +48,25 @@
                       <tr class="bg-gray-100 text-center border-b text-sm text-gray-600">
                           <td class="border-r">{{ $permit->id}}</td>
                           <td class="border-r">{{ $permit->user->firstName}} {{ $permit->user->lastName}}</td>
-                          <td class="border-r">{{ $permit->location}}</td>
                           <td class="border-r">{{ $permit->businessName}}</td>
+                          <td class="border-r">{{ $permit->location}}</td>
+                          <td class="border-r">
+                                <div class="md:w-1/2 lg:w-full px-3 mb-6 md:mb-0">
+                                    @if($permit->payment == 'pending')
+                                    <form method="POST" action="{{ route('permits.update', $permit->id) }}">
+                                        @method('PATCH')
+                                        @csrf
+                                        <button value="paid" name="payment" id="payment" class="bg-red-500 hover:bg-red-600 p-2 text-white hover:shadow-lg rounded-lg text-xs font-thin">
+                                            Click if paid
+                                        </button>
+                                    </form>
+                                    @else()
+                                        <button class="bg-green-600 hover:bg-green-700 p-2 text-white hover:shadow-lg rounded-lg text-xs font-thin">
+                                            paid
+                                        </button>
+                                    @endif
+                                </div>
+                          </td>
                           <td class="flex justify-center space-x-2 p-2">
                             <a href="{{ route('permits.show', $permit->id)}}">
                                 <button class="p-2 text-xs font-thin text-white bg-blue-500 rounded-lg hover:bg-blue-600 hover:shadow-lg">View</button>
